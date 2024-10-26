690af96f38effeecd02f42cf12ea8a90f029a091
cxdll <leding@unc.edu>
2024-10-25 21:36:51 -0400

    setup firebase

---
+++ teach.me-web/src/app/firebase/firebaseConfig.js
@@ -0,0 +1,18 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getDatabase } from "firebase/database";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCLf-oBLiMpIVYeM3Q3zv5Ob19qCdD11jM",
+  authDomain: "teachme-d2815.firebaseapp.com",
+  projectId: "teachme-d2815",
+  storageBucket: "teachme-d2815.appspot.com",
+  messagingSenderId: "676482642216",
+  appId: "1:676482642216:web:9e6e1bbf290bd0e988e01d",
+  measurementId: "G-841Y1THXRR"
+};
+
+const app = initializeApp(firebaseConfig);
+const database = getDatabase(app);
+
+export { database };
